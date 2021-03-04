783c9c3b26d579957671d0e7e9c586dce8ac0639
Danny van Os <dannyvanos93@gmail.com>
2021-03-04 22:49:26 +0100

    Created a view that shows the generated QR Code

---
+++ SeeSharpersCinema/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeeSharpersCinema.Models
+{
+    public class Ticket
+    {
+        public string Title { get; set; }
+        public DateTime TimeSlot { get; set; }
+        public int RoomID { get; set; }
+        public int SeatNum { get; set; }
+        public bool Discounted { get; set; }
+    }
+}
