56c19ac0e43bcca4ba4d8732a77abb7cbaaec129
Wesley van Burik <w.vanburik@student.avans.nl>
2021-03-07 20:35:34 +0100

    move mailservcie to order map

--- SeeSharpersCinema/Models/Order/MailService.cs
+++ SeeSharpersCinema/Models/Order/MailService.cs
@@ -13,28 +13,6 @@ namespace SeeSharpersCinema.Models
     public class EmailService
     {
         private const String cinemaEmail = "SeeSharpersCinema@gmail.com";
-        /*public void email_send()
-        {
-            MailMessage mail = new MailMessage();
-            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-            SmtpServer.UseDefaultCredentials = false;
-
-            mail.From = new MailAddress("wesleystudentavans@gmail.com");
-            mail.To.Add("w.vanburik@student.avans.nl");
-            mail.Subject = "Test Mail - 1";
-            mail.Body = "mail with attachment";
-
-            System.Net.Mail.Attachment attachment;
-            attachment = new System.Net.Mail.Attachment("C:/Users/WesleyB/Downloads/Microsoft.test-king.MB-500.v2020-06-02.by_.amelie.37q.pdf");
-            mail.Attachments.Add(attachment);
-
-            SmtpServer.Port = 587;
-            SmtpServer.Credentials = new System.Net.NetworkCredential("wesleystudentavans@gmail.com", "Wes4533b!");
-            SmtpServer.EnableSsl = true;
-
-            SmtpServer.Send(mail);
-
-        }*/
 
         public void email_send()
         {
