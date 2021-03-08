a1842de84d405a0ae083b52286508b86a1970be6
Danny van Os <dannyvanos93@gmail.com>
2021-03-08 20:34:49 +0100

    Added Ticket repo

---
+++ SeeSharpersCinema/Models/Repository/ITicketRepository.cs
@@ -0,0 +1,10 @@
+﻿using SeeSharpersCinema.Models.Order;
+using System.Linq;
+
+namespace SeeSharpersCinema.Models.Repository
+{
+    public interface ITicketRepository
+    {
+        IQueryable<Ticket> Tickets { get; }
+    }
+}
