f201619e9b81f3ab1fc5445e38ce7f5c7f285dde
Danny van Os <dannyvanos93@gmail.com>
2021-03-09 21:47:48 +0100

    Added the website with data from other project

---
+++ SeeSharpersCinema.Website/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SeeSharpersCinema.Models.Repository;
+using SeeSharpersCinema.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeeSharpersCinema.Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMovieRepository repository;
+        public HomeController(IMovieRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index() => View(new MovieListViewModel
+        {
+            Movies = repository.Movies.OrderBy(p => p.Id)
+        });
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ViewResult Overview()
+            => View(new MovieListViewModel
+            {
+                Movies = repository.Movies
+                .OrderBy(p => p.Id)
+            });
+
+        //public async Task<IActionResult> Overview()
+        //{
+        //    return View(await repository.FindAllAsync());
+        //}
+    }
+}
