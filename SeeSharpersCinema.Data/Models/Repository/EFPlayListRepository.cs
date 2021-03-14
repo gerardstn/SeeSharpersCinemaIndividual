e3b3390188733d932e018e5f07c7a2c481c0e0b9
Inge <57833480+Inge360@users.noreply.github.com>
2021-03-14 16:09:42 +0100

    Userselection movies finished. Unresolved: notification which selection is being asked.

--- SeeSharpersCinema.Data/Models/Repository/EFPlayListRepository.cs
+++ SeeSharpersCinema.Data/Models/Repository/EFPlayListRepository.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 
 namespace SeeSharpersCinema.Models.Repository
 {
+    /// <summary>
+    /// EFPlayListRepository queries and stores database data
+    /// </summary>
     public class EFPlayListRepository : IPlayListRepository
     {
         private CinemaDbContext context;
@@ -23,7 +26,7 @@ namespace SeeSharpersCinema.Models.Repository
         }
 
         /// <summary>
-        /// Method to find all movies in a task for correct threading
+        /// Queries the database to find all movies in a task for correct threading
         /// </summary>
         /// <returns>IEnumerable list of PlayList objects</returns>
         public async Task<IEnumerable<PlayList>> FindAllAsync()
@@ -49,6 +52,13 @@ namespace SeeSharpersCinema.Models.Repository
             .Where(z => z.TimeSlot.SlotStart.Date >= startDate && z.TimeSlot.SlotStart.Date <= endDate)
             .ToListAsync();
 
+        /// <summary>
+        /// Queries the database to find a movie by title in a task for correct threading
+        /// </summary>
+        /// <param name="startDate">From when it should add movies to the list. This is defined by the method in HomeController.</param>
+        /// <param name="endDate">Untill what date it should add movies to the list. This is defined by the method in HomeController.</param>
+        /// <param name="uiTitle">Selection string: user input title from HomeController</param>
+        /// <returns>Movie with this title</returns>
         public async Task<IEnumerable<PlayList>> FindByTitle(DateTime startDate, DateTime endDate, string uiTitle)
             => await context.PlayLists
             .Include(b => b.Movie)
@@ -58,6 +68,11 @@ namespace SeeSharpersCinema.Models.Repository
             .Where(z => z.TimeSlot.SlotStart.Date >= startDate && z.TimeSlot.SlotStart.Date <= endDate && z.Movie.Title.Contains(uiTitle))
             .ToListAsync();
 
+        /// <summary>
+        /// Queries the database to find movies by date in a task for correct threading
+        /// </summary>
+        /// <param name="uiDate">Selection string: user input DateTime date from HomeController</param>
+        /// <returns>Movies shown at this date</returns>
         public async Task<IEnumerable<PlayList>> FindByDate(DateTime uiDate)
             => await context.PlayLists
             .Include(b => b.Movie)
@@ -67,6 +82,13 @@ namespace SeeSharpersCinema.Models.Repository
             .Where(z => z.TimeSlot.SlotStart.Date == uiDate)
             .ToListAsync();
 
+        /// <summary>
+        /// Queries the database to find movies by genre in a task for correct threading
+        /// </summary>
+        /// <param name="startDate">From when it should add movies to the list. This is defined by the method in HomeController.</param>
+        /// <param name="endDate">Untill what date it should add movies to the list. This is defined by the method in HomeController.</param>
+        /// <param name="uiGenre">Selection string: user input genre from HomeController</param>
+        /// <returns>Movies with this genre</returns>
         public async Task<IEnumerable<PlayList>> FindByGenre(DateTime startDate, DateTime endDate, string uiGenre)
             => await context.PlayLists
             .Include(b => b.Movie)
@@ -77,6 +99,12 @@ namespace SeeSharpersCinema.Models.Repository
                     z.Movie.Genre.Equals(Enum.Parse(typeof(Genre), uiGenre)))
             .ToListAsync();
 
+        /// <summary>
+        /// Queries the database to find movies by title and date in a task for correct threading
+        /// </summary>
+        /// <param name="uiTitle">Selection string: user input title from HomeController</param>
+        /// <param name="uiDate">Selection string: user input DateTime date from HomeController</param>
+        /// <returns>Movies with this title shown at this date</returns>
         public async Task<IEnumerable<PlayList>> FindByTitleAndDate(string uiTitle, DateTime uiDate)
             => await context.PlayLists
             .Include(b => b.Movie)
@@ -86,6 +114,12 @@ namespace SeeSharpersCinema.Models.Repository
             .Where(z => z.Movie.Title.Contains(uiTitle) && z.TimeSlot.SlotStart.Date == uiDate)
             .ToListAsync();
 
+        /// <summary>
+        /// Queries the database to find movies by date and genre in a task for correct threading
+        /// </summary>
+        /// <param name="uiDate">Selection string: user input DateTime date from HomeController</param>
+        /// <param name="uiGenre">Selection string: user input DateTime date from HomeController</param>
+        /// <returns>Movies shown at this date with this genre</returns>
         public async Task<IEnumerable<PlayList>> FindByDateAndGenre(DateTime uiDate, string uiGenre)
             => await context.PlayLists
             .Include(b => b.Movie)
@@ -95,6 +129,13 @@ namespace SeeSharpersCinema.Models.Repository
             .Where(z => z.TimeSlot.SlotStart.Date == uiDate && z.Movie.Genre.Equals(Enum.Parse(typeof(Genre), uiGenre)))
             .ToListAsync();
 
+        /// <summary>
+        /// Queries the database to find movies by view indication in a task for correct threading
+        /// </summary>
+        /// <param name="startDate">From when it should add movies to the list. This is defined by the method in HomeController.</param>
+        /// <param name="endDate">Untill what date it should add movies to the list. This is defined by the method in HomeController.</param>
+        /// <param name="uiViewIndication">Selection string: user input view indication from HomeController</param>
+        /// <returns></returns>
         public async Task<IEnumerable<PlayList>> FindByViewIndication(DateTime startDate, DateTime endDate, string uiViewIndication)
             => await context.PlayLists
             .Include(b => b.Movie)
@@ -105,6 +146,12 @@ namespace SeeSharpersCinema.Models.Repository
                     z.Movie.ViewIndication.Equals(Enum.Parse(typeof(ViewIndication), uiViewIndication)))
             .ToListAsync();
 
+        /// <summary>
+        /// Queries the database to find movies shown at this date with this view indication in a task for correct threading
+        /// </summary>
+        /// <param name="uiDate">Selection string: user input DateTime date from HomeController</param>
+        /// <param name="uiViewIndication">Selection string: user input view indication from HomeController</param>
+        /// <returns>Movies shown at this date with this view indication</returns>
         public async Task<IEnumerable<PlayList>> FindByDateAndViewIndication(DateTime uiDate, string uiViewIndication)
             => await context.PlayLists
             .Include(b => b.Movie)
@@ -114,6 +161,14 @@ namespace SeeSharpersCinema.Models.Repository
             .Where(z => z.TimeSlot.SlotStart.Date == uiDate && z.Movie.ViewIndication.Equals(Enum.Parse(typeof(ViewIndication), uiViewIndication)))
             .ToListAsync();
 
+        /// <summary>
+        /// Queries the database to find movies with this view indication and genre in a task for correct threading
+        /// </summary>
+        /// <param name="startDate">From when it should add movies to the list. This is defined by the method in HomeController.</param>
+        /// <param name="endDate">Untill what date it should add movies to the list. This is defined by the method in HomeController.</param>
+        /// <param name="uiViewIndication">Selection string: user input view indication from HomeController</param>
+        /// <param name="uiGenre">Selection string: user input DateTime date from HomeController</param>
+        /// <returns></returns>
         public async Task<IEnumerable<PlayList>> FindByViewIndicationAndGenre(DateTime startDate, DateTime endDate, string uiViewIndication, string uiGenre)
            => await context.PlayLists
            .Include(b => b.Movie)
@@ -124,6 +179,5 @@ namespace SeeSharpersCinema.Models.Repository
                    z.Movie.ViewIndication.Equals(Enum.Parse(typeof(ViewIndication), uiViewIndication)) &&
                    z.Movie.Genre.Equals(Enum.Parse(typeof(Genre), uiGenre)))
            .ToListAsync();
-
     }
 }
