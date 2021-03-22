962608a1b851b6bbb17ed995de6e2e966d49001f
Ammar Alkhatib <ammar.alkhatib88@gmail.com>
2021-03-22 23:35:46 +0100

    Mollie package deleted to work on ther branches

--- SeeSharpersCinema.Website/Controllers/PaymentController.cs
+++ SeeSharpersCinema.Website/Controllers/PaymentController.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SeeSharpersCinema.Models.Order;
-using Mollie.Api.Client;
+/*using Mollie.Api.Client;
 using Mollie.Api.Client.Abstract;
 using Mollie.Api.Models;
 using Mollie.Api.Models.Payment.Request;
-using Mollie.Api.Models.Payment.Response;
+using Mollie.Api.Models.Payment.Response;*/
 using SeeSharpersCinema.Models.Film;
 using System;
 using System.Collections.Generic;
@@ -19,14 +19,14 @@ namespace SeeSharpersCinema.Website.Controllers
 
     public class PaymentController : Controller
     {
-        IPaymentClient paymentClient = new PaymentClient("test_rWeMRKpke8RHJFezCqenNyJmHtQry8");
+   /*     IPaymentClient paymentClient = new PaymentClient("test_rWeMRKpke8RHJFezCqenNyJmHtQry8");
         PaymentRequest paymentRequest = new PaymentRequest()
         {
             Amount = new Amount(Currency.EUR, 100.00m),
             Description = "Test payment of the example project",
             RedirectUrl = "http://google.com",
             Method = Mollie.Api.Models.Payment.PaymentMethod.Ideal // instead of "Ideal"
-        };
+        };*/
         /*        PaymentResponse paymentResponse = await paymentClient.CreatePaymentAsync(paymentRequest);
         */
 
