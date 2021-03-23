af2baa7137e6cc2fc99ff4a3481338b4a8828e35
Noel Bissels <mail@freckled.nl>
2021-03-23 11:17:00 +0100

    added docs, moves some stuff around

--- SeeSharpersCinema.Data/Models/Repository/IReservedSeatRepository.cs
+++ SeeSharpersCinema.Data/Models/Repository/IReservedSeatRepository.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 
 namespace SeeSharpersCinema.Data.Models.Repository
 {
+
+    /// <summary>
+    /// IReservedSeatRepository interface makes sure these methods are implemented
+    /// in a tasks for correct threading
+    /// </summary>
+    /// <returns>IEnumerable<ReservedSeat> objects or ICollection<ReservedSeat> object</returns>
+
     public interface IReservedSeatRepository: IRepository<ReservedSeat>
     {
         public Task<IEnumerable<ReservedSeat>> FindAllByTimeSlotIdAsync(long TimeslotId);
