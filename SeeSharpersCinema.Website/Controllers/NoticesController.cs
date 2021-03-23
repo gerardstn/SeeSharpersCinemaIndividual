79309089b549255e2a361334e0f66fea6d8d7516
Gerard <g.sintnicolaas@student.avans.nl>
2021-03-23 09:01:24 +0100

    Documentatie toegevoegd bij noticecontroller.

--- SeeSharpersCinema.Website/Controllers/NoticesController.cs
+++ SeeSharpersCinema.Website/Controllers/NoticesController.cs
@@ -16,27 +16,11 @@ namespace SeeSharpersCinema.Website.Controllers
             _context = context;
         }
 
-
-        // GET: Notices/Details/5
-        public async Task<IActionResult> Details(long? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var notice = await _context.Notices
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (notice == null)
-            {
-                return NotFound();
-            }
-
-            return View(notice);
-        }
-
-
-        // GET: Notices/Edit/5
+        /// <summary>
+        /// This finds the notice with the message so that you are able to edit it.
+        /// </summary>
+        /// <param name="id">id of the notice</param>
+        /// <returns>Message of the notice</returns>
         public async Task<IActionResult> Edit(long? id)
         {
             if (id == null)
@@ -52,9 +36,12 @@ namespace SeeSharpersCinema.Website.Controllers
             return View(notice);
         }
 
-        // POST: Notices/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Adding the ability to save the new notice to the server.
+        /// </summary>
+        /// <param name="id">Primary key of notice, to find it</param>
+        /// <param name="notice">The notice model, which consists of an id, message and isActive bool</param>
+        /// <returns>Editable notice, which saves to the database when you submit</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(long id, [Bind("Id,Message,isActive")] Notice notice)
@@ -86,7 +73,11 @@ namespace SeeSharpersCinema.Website.Controllers
             }
             return View(notice);
         }
-
+        /// <summary>
+        /// A helper for the method above, to make sure the notice exists.
+        /// </summary>
+        /// <param name="id">primary key of the notice</param>
+        /// <returns>true or false depending on if a notice exists</returns>
         private bool NoticeExists(long id)
         {
             return _context.Notices.Any(e => e.Id == id);
