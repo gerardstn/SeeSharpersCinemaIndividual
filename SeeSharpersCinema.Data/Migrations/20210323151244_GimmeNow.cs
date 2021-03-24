85d58d5847433ac13fd8ff75e5de6e3d01f9942d
Inge <57833480+Inge360@users.noreply.github.com>
2021-03-24 09:08:39 +0100

    Database Update

---
+++ SeeSharpersCinema.Data/Migrations/20210323151244_GimmeNow.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SeeSharpersCinema.Data.Migrations
+{
+    public partial class GimmeNow : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
